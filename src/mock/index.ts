25b5db25e584aa892583c3f7e5290b19e40393c8
Nikita Kovalyov <boba.bibap@gmail.com>
2023-06-30 18:51:02 +0300

    finished

---
+++ src/mock/index.ts
@@ -0,0 +1,16 @@
+import { LocalGithubUser } from "types"
+
+export const defaultUser: LocalGithubUser = {
+  login: "ItCreatorNik",
+  avatar: "https://avatars.githubusercontent.com/u/96583901?v=4",
+  name: "Nikita",
+  company: "In search",
+  blog: "https://github.com/ItCreatorNik",
+  location: "Kyiv",
+  bio: "React developer",
+  twitter: "@twitter",
+  repos: 22,
+  followers: 1,
+  following: 1,
+  created: "2021-12-23T14:29:20Z",
+};  
